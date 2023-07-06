c1b326ddf6cee6ea6e51e4dcc1ec13eb201af33a
Batuh <200205011@ostimteknik.edu.tr>
2023-07-06 20:08:49 +0300

    Singlegamebot

--- QuizMaster/Assets/Code/V2/InfoRoomReturn.cs
+++ QuizMaster/Assets/Code/V2/InfoRoomReturn.cs
@@ -10,4 +10,14 @@ public class InfoRoomSec
     public string status;
     public int kisiSayisi;
 }
+public class InfoBotRoomSec
+{
+    public string isPlayer;
+    public string Player;
+    public int puan;
+    public string word;
+    public int tour;
+    public bool isGameStarted;
+    public string status;
+}
 
