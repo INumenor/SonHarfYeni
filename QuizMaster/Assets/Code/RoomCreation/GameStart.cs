eeb58ddf83461ec52686b14e4f84143f2cca0a0e
Batuh <200205011@ostimteknik.edu.tr>
2023-07-19 01:19:20 +0300

    abov

--- QuizMaster/Assets/Code/RoomCreation/GameStart.cs
+++ QuizMaster/Assets/Code/RoomCreation/GameStart.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.Networking;
 using System.Text;
-using TMPro;
-using System;
-using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameStart : MonoBehaviour
 {
     [SerializeField] GameObject LobbyExitButton;
@@ -42,6 +40,8 @@ public class GameStart : MonoBehaviour
     public void LobbyQuit()
     {
         StartCoroutine(Post("http://localhost:8080/ServiceKelimeOyunu/Service/quitGame", processJson(GlobalKullanıcıBilgileri._OyuncuIsim, GlobalKullanıcıBilgileri._Room_key)));
+        GlobalKullanıcıBilgileri._Room_key = null;
+        SceneManager.LoadScene(1);
     }
     IEnumerator Post(string url, string bodyJsonString)
     {
@@ -57,58 +57,30 @@ public class GameStart : MonoBehaviour
     private void processJsonData(string req)
     {
         LobbyInfoSQL lobby = JsonUtility.FromJson<LobbyInfoSQL>(req);
-        if (Flag == 0)
+        string[] nokta = { ";;;" };
+        string[] players = lobby.Players.Split(nokta, System.StringSplitOptions.RemoveEmptyEntries);
+        if (players.Length != clones.Count)
         {
             RoomName.GetComponent<Text>().text = "Oda İsmi :" + lobby.RoomName.ToString();
             RoomKey.GetComponent<Text>().text = lobby.RoomKey.ToString();
-
-            string[] nokta = { ";;;" };
-            string[] players = lobby.Players.Split(nokta, System.StringSplitOptions.RemoveEmptyEntries);
-
             RoomPlayers.GetComponent<Text>().text = "Oyuncu Sayısı/Oda Limiti :" + players.Length + "/" + lobby.NumberOfPeople;
             RoomType.GetComponent<Text>().text = lobby.isPrivate.ToString();
             isTime.GetComponent<Text>().text = lobby.Time.ToString();
             PuanType.GetComponent<Text>().text = lobby.PointType.ToString();
-            RawImage.GetComponentInChildren<Text>().text = players[0];
-            RawImage.name = players[0];
+            //RawImage.GetComponentInChildren<Text>().text = players[0];
+            //RawImage.name = players[0];
             CopyRawImage = RawImage;
-            clones.Add(CopyRawImage);
-            for (int i = 1; i < players.Length; i++)
+            //clones.Add(CopyRawImage);
+            for (int i = 0; i < clones.Count; i++)
             {
-                GameObject clone = Instantiate(CopyRawImage, new Vector3(CopyRawImage.transform.position.x, CopyRawImage.transform.position.y - 1.4f, CopyRawImage.transform.position.z), CopyRawImage.transform.rotation);
-                Transform parentTransform = clone.transform;
-                string[] cloneplayers = lobby.Players.Split(nokta, System.StringSplitOptions.RemoveEmptyEntries);
-                clone.GetComponentInChildren<Text>().text = players[i];
-                clone.name = players[i];
-                clone.transform.parent = Content.transform;
-                clone.transform.localScale = new Vector3(1, 0.5f, 1);
-                CopyRawImage = clone;
-                clones.Add(clone);
+                Destroy(clones[i]);
             }
-        }
-        else
-        {
-            string[] nokta = { ";;;" };
-            string[] players = lobby.Players.Split(nokta, System.StringSplitOptions.RemoveEmptyEntries);
-            int iBrake = 0;
-            Debug.Log(players.Length+"a"+clones.Count);
-            if (players.Length != clones.Count)
-            {
-                iBrake = 1;
-            }
-            if(iBrake == 1) 
-            {
-                for (int i = 1; i < clones.Count; i++)
-                {
-                    DestroyImmediate(clones[i]);
-                    clones.Remove(clones[i]);
-                }
-                CopyRawImage = RawImage;
-                for (int i = 1; i < players.Length; i++)
+            clones.Clear();
+                for (int i = 0; i < players.Length; i++)
                 {
                     GameObject clone = Instantiate(CopyRawImage, new Vector3(CopyRawImage.transform.position.x, CopyRawImage.transform.position.y - 1.4f, CopyRawImage.transform.position.z), CopyRawImage.transform.rotation);
                     Transform parentTransform = clone.transform;
-                    string[] cloneplayers = lobby.Players.Split(nokta, System.StringSplitOptions.RemoveEmptyEntries);
+                    clone.active = true;
                     clone.GetComponentInChildren<Text>().text = players[i];
                     clone.name = players[i];
                     clone.transform.parent = Content.transform;
@@ -116,9 +88,7 @@ public class GameStart : MonoBehaviour
                     CopyRawImage = clone;
                     clones.Add(clone);
                 }
-            }
         }
-        Flag = 1;
     }
 
     private string processJson(string _url, string room_key)
